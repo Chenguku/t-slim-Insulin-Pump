739d65da8eaf6204fa901e125c3f73f4eb005ad6
stanleyyellowzx <stanleyhuang0702@gmail.com>
2025-04-18 12:29:08 -0400

    added popup message for low battery

--- tslimSim/mainwindow.cpp
+++ tslimSim/mainwindow.cpp
@@ -589,6 +589,7 @@ void MainWindow::increaseBattery(){
 }
 
 void MainWindow::createLowBatteryEvent(){
+    //message box creates a message popup to tell the user their device battery is low
     QMessageBox::information(this, "Low Battery", "Your Device is Low on Battery", QMessageBox::Ok);
     events.addEvent(new AlertEvent(simulationTime, "Low Battery"));
 }
