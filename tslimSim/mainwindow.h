ba181c2f915b37b50caffa4188499b580fdf7722
Chenguku <peasantrule@gmail.com>
2025-04-18 21:15:20 -0400

    migrated adjustBG to effect model

--- tslimSim/mainwindow.h
+++ tslimSim/mainwindow.h
@@ -67,7 +67,6 @@ private:
     BolusCalculator     *bolusCalc;
     int                 passcode;
     EventHistory        events;
-    Profile             *curProfile;
     ProfilesPageWidget  *profilesPageWidget;
     ProfileFormWidget   *profileFormWidget;
     std::vector<Event*> recentEvents;
